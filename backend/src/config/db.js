ee8ecd037f487df951d7437b7bda8671fd5752b2
rickyveeld <rickyvelediaz@gmail.com>
2026-04-01 19:59:32 -0600

    fix: remove invalid comment in db config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log("DATABASE_URL existe?", !!process.env.DATABASE_URL);
+
 const pool = new Pool({
-    connectionString:process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
